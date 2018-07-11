846e49ae6d212408f7ff43fc3138ff8d364d0f43
ajaykpat <ajaykumar.patel@gmail.com>
2018-07-11 00:07:32 -0300

    Add files via upload

---
+++ socket1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import socket
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET https://hanumanvirtual.wordpress.com/ HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
